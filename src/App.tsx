000133b7e04b7b15d022ddd69b3b5f6e0070c4c9
Jon Naske <jonnaske@hotmail.com>
2024-10-12 21:47:01 -0700

    implemented tailwind and shadcn. removed scaffolded styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import { ModeToggle } from "./components/mode-toggle"
 
 function App() {
-  const [count, setCount] = useState(0)
   const [schema, setSchema] = useState("")
 
   useEffect(() => {
@@ -17,29 +14,10 @@ function App() {
   console.log(schema)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <pre style={{ textAlign: "left" }}>{schema}</pre>
-    </>
+    <div className="dark">
+      <ModeToggle />
+      <pre className="dark">{schema}</pre>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./styles/globals.css"
+import { ThemeProvider } from "./components/theme-provider.tsx"
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
